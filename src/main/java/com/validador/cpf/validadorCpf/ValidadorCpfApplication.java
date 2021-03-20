c6ee838f400d45255102c8dd620c7791c156c8b9
stehleao <stephanieleaof@gmail.com>
2021-03-20 11:02:06 -0300

    first commit

---
+++ src/main/java/com/validador/cpf/validadorCpf/ValidadorCpfApplication.java
@@ -0,0 +1,12 @@
+package com.validador.cpf.validadorCpf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValidadorCpfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValidadorCpfApplication.class, args);
+	}
+}
